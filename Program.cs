457e33ca8811b9c7c9684e19727f4ecfab88a9f4
RobertoBaiochi <robertombbaiocho@gmail.com>
2024-02-20 09:38:31 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Globalization;
+
+namespace AplicativoIMC
+{
+    internal class Program
+    {
+        struct Pessoa
+        {
+            public double altura, peso;
+        }
+
+        static void Main(string[] args)
+        {
+
+            double imc;
+
+            Console.Write("Digite sua altura: ");
+            double alturaEntrada = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Digite seu peso: ");
+            double pesoEntrada = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Pessoa pessoa = new Pessoa()
+            {
+                altura = alturaEntrada,
+                peso = pesoEntrada,
+
+            };
+
+            imc = CalcularIMC(pessoa.altura, pessoa.peso);
+
+            if (imc < 16)
+            {
+                Console.WriteLine("Subpeso Severo");
+            }
+            else if(imc <= 19.9)
+            {
+                Console.WriteLine("Subpeso");
+            }
+            else if(imc <= 24.9)
+            {
+                Console.WriteLine("Normal");
+            }
+            else if(imc <= 29.9)
+            {
+                Console.WriteLine("Sobrepeso");
+            }
+            else if(imc <= 39.9)
+            {
+                Console.WriteLine("Obeso");
+            }
+            else
+            {
+                Console.WriteLine("Obeso Mórbido");
+            }
+
+        }
+
+        static double CalcularIMC(double altura, double peso)
+        {
+            return peso / Math.Pow(altura, 2);
+        }
+    }
+}
